fe2277dd889c85eecc431ac348ae75c2539349e9
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-02-18 00:25:04 +0600

    logotype component was done

---
+++ src/components/Logotype/Logotype.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from './Logotype.module.css';
+
+import logoIcon from '../../assetes/logotype-icon.png';
+
+interface Props { };
+
+const Logotype: React.FC<Props> = (props): JSX.Element => {
+   return (
+      <NavLink to='/' className={s.content}>
+         <div className={s.image}>
+            <img src={logoIcon} className={s.img} />
+         </div>
+         <span className={s.text}>Pokebook</span>
+      </NavLink>
+   );
+}
+
+
+export default Logotype;
